4643a5d0200d5f26e53a70c4f938ea8dc1fa383a
Gyanaranjan11 <iamgyana5@gmail.com>
2022-07-27 21:34:05 +0530

    test1

--- StudentinfoApp/forms.py
+++ StudentinfoApp/forms.py
@@ -8,7 +8,16 @@ class StudentInfoFrom(forms.ModelForm):
     class Meta:
         model = StudentInfo
         fields = '__all__'
+class StudentAcademic(models.Model):
+    roll_no = models.ForeignKey(StudentInfo, on_delete=models.CASCADE)
+    math = models.IntegerField(null=True, blank=True)
+    physic = models.IntegerField(null=True, blank=True)
+    chemistry = models.IntegerField(null=True, blank=True)
+    english = models.IntegerField(null=True, blank=True)
+    biology = models.IntegerField(null=True, blank=True)
 
+    def __str__(self):
+        return self.roll_no.name
 
 class StudentAcademicFrom(forms.ModelForm):
     class Meta:
