55883738a5567e8c04b6963955a355fd00f9cd96
Gyanaranjan11 <97451360+Gyanaranjan11@users.noreply.github.com>
2022-07-27 21:41:32 +0530

    Revert "4 test3 data"

--- StudentinfoApp/forms.py
+++ StudentinfoApp/forms.py
@@ -8,8 +8,6 @@ class StudentInfoFrom(forms.ModelForm):
     class Meta:
         model = StudentInfo
         fields = '__all__'
-    roll_no = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=50, null=True, blank=True)
 class StudentAcademic(models.Model):
     roll_no = models.ForeignKey(StudentInfo, on_delete=models.CASCADE)
     math = models.IntegerField(null=True, blank=True)
@@ -35,14 +33,3 @@ class StudentAcademic(models.Model):
 
     def __str__(self):
         return self.roll_no.name
-    chemistry = models.IntegerField(null=True, blank=True)
-    english = models.IntegerField(null=True, blank=True)
-    biology = models.IntegerField(null=True, blank=True)
-
-    def __str__(self):
-        return self.roll_no.name
-    
-class StudentInfo(models.Model):
-    roll_no = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=50, null=True, blank=True)
-    sclass = models.CharField(max_length=50, null=True, blank=True)
