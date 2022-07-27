1a36e95f7b81a4210f3d18f516c47f1f77b2f296
Gyanaranjan11 <iamgyana5@gmail.com>
2022-07-27 21:12:08 +0530

    test data

--- StudentinfoApp/apps.py
+++ StudentinfoApp/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class StudentinfoappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'StudentinfoApp'
+
+def test():
+    print("_----------------------")
+    print("gyana")
+    print("_----------------------")
+    print("gyana")
