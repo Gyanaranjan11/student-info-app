0a928894143052d7ffe09f525859cdff42608bb6
Gyanaranjan11 <iamgyana5@gmail.com>
2022-07-27 21:15:13 +0530

    test2

--- StudentinfoApp/apps.py
+++ StudentinfoApp/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class StudentinfoappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'StudentinfoApp'
+class StudentAcademic(models.Model):
+    roll_no = models.ForeignKey(StudentInfo, on_delete=models.CASCADE)
+    math = models.IntegerField(null=True, blank=True)
+    physic = models.IntegerField(null=True, blank=True)
+    chemistry = models.IntegerField(null=True, blank=True)
+    english = models.IntegerField(null=True, blank=True)
+    biology = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.roll_no.name
